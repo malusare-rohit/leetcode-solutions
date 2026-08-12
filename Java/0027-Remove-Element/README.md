8a0c9c077ded997fca69b24682e94d390eee094f
malusare-rohit <malusarerohit500@gmail.com>
2026-08-12 22:05:08 +0530

    added Remove Element Solution

---
+++ Java/0027-Remove-Element/README.md
@@ -0,0 +1,19 @@
+# 0027. Remove Element
+
+## Topics
+- Array (Two Pointers Approach)
+
+## Difficulty
+Easy
+
+## Solutions
+
+### Solution 1
+
+Runtime 0 ms Beats 100.00%
+Memory 43.56 MB Beats 43.63%
+
+Time: O(N)
+
+Space: O(1)
+
---
+++ Java/0027-Remove-Element/Solution1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        
+        for(int i=0;i<nums.length;i++){
+             if(nums[i]==val){
+                continue;
+             }
+             if(i!=k){
+                nums[k]=nums[i];
+             }
+             k++;
+        }
+        return k;
+    }
+}
