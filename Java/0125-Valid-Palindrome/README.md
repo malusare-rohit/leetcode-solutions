5d355b9c4b6f2c73cb6041bde57457b2d9842692
malusare-rohit <malusarerohit500@gmail.com>
2026-08-17 23:29:17 +0530

    added Valid Palindrome Solution

---
+++ Java/0125-Valid-Palindrome/README.md
@@ -0,0 +1,19 @@
+# 0125. Valid Palindrome
+
+## Topics
+- Two Pointers/String
+
+## Difficulty
+Easy
+
+## Solutions
+
+### Solution 1
+
+Runtime 2 ms Beats 99.32%
+Memory 44.47 MB Beats 54.50%
+
+Time: O(N)
+
+Space: O(1)
+
---
+++ Java/0125-Valid-Palindrome/Solution.java
@@ -0,0 +1,32 @@
+package Java.0125-Valid-Palindrome;
+
+public class Solution {
+    
+}
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) !=
+                Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
